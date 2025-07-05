4fed0d226a2c15a0e05fa3031d191b6f97f68560
Florian Köhler <git@flokoe.de>
2025-07-05 16:03:20 +0200

    Add Logger and Recover middleware

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
 
 	e.Static("/", "public")
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
 		return c.HTML(http.StatusOK, "<!doctype html><html><head></head><body><h1>Hello World!</h1></body></html>")
