0fec0d2a592fafccaac82870e5d3eb1c1f6e41b5
Florian Köhler <git@flokoe.de>
2025-07-05 15:51:05 +0200

    Enable serving static files

--- main.go
+++ main.go
@@ -9,8 +9,12 @@ import (
 
 func main() {
 	e := echo.New()
+
+	e.Static("/", "public")
+
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.HTML(http.StatusOK, "<!doctype html><html><head></head><body><h1>Hello World!</h1></body></html>")
 	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
